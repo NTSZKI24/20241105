dfbb30b40396079c9d347bf0f83f875d816e4085
RiXXXhun <100133611+RiXXXhun@users.noreply.github.com>
2024-11-05 16:41:04 +0100

    init

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,18 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 
+app.get("/loop:i", (req, res) => {
+    const i = parseInt(req.params.i) // bekert ertek
+    const tomb: string[] = [];
+
+    for (let j = 0; j < i; j++) {
+        tomb.push(`Elem ${j + 1}`);
+    }
+
+    res.json(tomb)
+    return
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`server is running on port: ${process.env.PORT}`)
     AppDataSource.initialize()
