a607903141b63d011a59546a8dfb9a4393755458
RiXXXhun <100133611+RiXXXhun@users.noreply.github.com>
2024-11-07 16:49:13 +0100

    aaa

---
+++ src/routes/news.controller.ts
@@ -0,0 +1,25 @@
+import express, {Request, Response} from "express"
+import { createNews, deleteNews, getAllNews, updateNews } from "../services/news.service";
+
+
+const router = express.Router()
+
+
+router.get("/", async (req: Request, res: Response) => {
+    
+})
+
+router.post("/create", async (req: Request, res: Response) => {
+    
+})
+
+router.put("/update/:id", async (req: Request, res: Response) => {
+    
+})
+
+router.delete("/delete/:id", async (req: Request, res: Response) => {
+   
+})
+
+export default router;
+
